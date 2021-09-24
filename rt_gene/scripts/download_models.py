778646ea7ea47f93d51ed2937ad32c7e51e48f42
memoryslices <memoryslices@foxmail.com>
2021-09-24 14:45:15 +0000

    to pc

--- rt_gene/scripts/download_models.py
+++ rt_gene/scripts/download_models.py
@@ -4,14 +4,16 @@
 # (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
 
 from __future__ import print_function, division, absolute_import
+import sys
+sys.path.append("/home/student/dms/rt_gene/rt_gene/src/rt_gene")
 
-import src.rt_gene.download_tools as download_tools
+import download_tools
 
 
 if __name__ == '__main__':
-    download_tools.download_gaze_tensorflow_models()
+    # download_tools.download_gaze_tensorflow_models()
     download_tools.download_gaze_pytorch_models()
-    download_tools.download_blink_tensorflow_models()
+    # download_tools.download_blink_tensorflow_models()
     download_tools.download_blink_pytorch_models()
     download_tools.download_external_landmark_models()
 
